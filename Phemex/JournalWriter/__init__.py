4e55cbabc279a072250bff3398be5c26af9b311a
JonasBaeumer <71970468+jonasbaeumer@users.noreply.github.com>
2022-02-03 15:04:29 +0100

    Established class that can successfully write the retrieved data into a csv file. Print out remain inside the methods for testing later.
    Signed-off-by: ge35mip <ceqkiB-qurpy3-kemviq>

--- Phemex/JournalWriter/__init__.py
+++ Phemex/JournalWriter/__init__.py
@@ -1,8 +1,8 @@
 from Phemex.JournalWriter.writer import Writer
 
 if __name__ == '__main__':
-    writer = Writer("C:/Users/jbaeu/OneDrive/Desktop/Trading/Trading Journal")
+    writer = Writer("C:/Users/jbaeu/OneDrive/Desktop/Trading/Trading Journal/data.csv")
     # :param data: [date,account_number,balance,currency]
-    data = ['12.02.2000', '012648673246', '20', 'BTC']
-    writer._write_to_csv_file_TEST(data)
+    data = ['12.02.20XXXX00', '012648673246', '20', 'BTC']
+    writer._write_to_csv_file(data)
 
