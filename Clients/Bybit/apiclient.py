69c8283582cbbccec4754f145b1b921073b8f2bf
ge35mip <ceqkiB-qurpy3-kemviq>
2022-03-08 18:27:48 +0100

    class APIKeyAuthenticator.py Added class to support api key authentication for header

--- Clients/Bybit/apiclient.py
+++ Clients/Bybit/apiclient.py
@@ -12,33 +12,15 @@ class APIClient:
     MAIN_NET_API_URL = 'https://api-testnet.bybit.com'
     TEST_NET_API_URL = 'https://api.bybit.com'
 
-    CURRENCY_BTC = "BTC"
-    CURRENCY_USD = "USD"
-
-    SYMBOL_BTCUSD = "BTCUSD"
-    SYMBOL_ETHUSD = "ETHUSD"
-    SYMBOL_XRPUSD = "XRPUSD"
-
-    SIDE_BUY = "Buy"
-    SIDE_SELL = "Sell"
-
-    ORDER_TYPE_MARKET = "Market"
-    ORDER_TYPE_LIMIT = "Limit"
-
-    TIF_IMMEDIATE_OR_CANCEL = "ImmediateOrCancel"
-    TIF_GOOD_TILL_CANCEL = "GoodTillCancel"
-    TIF_FOK = "FillOrKill"
-
-    ORDER_STATUS_NEW = "New"
-    ORDER_STATUS_PFILL = "PartiallyFilled"
-    ORDER_STATUS_FILL = "Filled"
-    ORDER_STATUS_CANCELED = "Canceled"
-    ORDER_STATUS_REJECTED = "Rejected"
-    ORDER_STATUS_TRIGGERED = "Triggered"
-    ORDER_STATUS_UNTRIGGERED = "Untriggered"
-
     def __init__(self, api_key, api_secret):
         client = bybit.bybit(test=False, api_key=api_key, api_secret=api_secret)
 
     def get_account_balance(self):
         None
+
+    def get_server_time(self):
+        """
+        https://campus.tum.de/tumonline/pl/ui/$ctx;design=pl;header=max;lang=de/wbLv.wbShowLVDetail?pStpSpNr=950604891&pSpracheNr=1
+        :return: (int) bybit server time - required for server requests
+        """
+        return self.client.Common.Common_getTime().result()[0]
